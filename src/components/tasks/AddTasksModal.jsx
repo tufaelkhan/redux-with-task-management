1f8a57efe3221fa2c7ab5daf1218755938833d59
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-27 07:22:52 -0700

    add modal

--- src/components/tasks/AddTasksModal.jsx
+++ src/components/tasks/AddTasksModal.jsx
@@ -1,9 +1,60 @@
+import { useForm } from "react-hook-form";
 import Modal from "../ui/Modal";
 
 function AddTasksModal({isOpen, setIsOpen}) {
+const { register, handleSubmit } = useForm()
+
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
+
     return (
-        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title='tufael khan'><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, commodi.
-                </p></Modal>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title='tufael khan'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-col gap-3">
+                <label htmlFor="title">Title</label>
+                <input className="w-full rounded-lg" type="text" id="title" {...register('name')} />
+               
+                <label htmlFor="title">Title</label>
+                <input className="w-full rounded-lg" type="text" id="title" {...register('name')} />
+                
+                <label htmlFor="title">Title</label>
+                <input className="w-full rounded-lg" type="text" id="title" {...register('name')} />
+                
+                <label htmlFor="title">Title</label>
+                <input className="w-full rounded-lg" type="text" id="title" {...register('name')} />
+                
+                <label htmlFor="title">Title</label>
+                <input className="w-full rounded-lg" type="text" id="title" {...register('name')} />
+                
+                <label htmlFor="title">Title</label>
+                <input className="w-full rounded-lg" type="text" id="title" {...register('name')} />
+               
+                <label htmlFor="title">Title</label>
+                <select className="w-full rounded-lg" id="assignedTo" {...register('assignedTo')}>
+                <option value="Tufael khan">Tufael Khan</option>
+                <option value="sujoy">sujoy</option>
+                <option value="sonjoy">sonjoy</option>
+                <option value="rakib">rakib</option>
+                <option value="jahid">jahid</option>
+                <option value="habib">habib</option>
+                <option value="polash">polash</option>
+                </select>
+                    </div>
+                    <div>
+                        <label htmlFor="title" className="mb-2">priority</label>
+                        <select className="w-full rounded-lg" id="priority" {...register('priority')}>
+                        <option value="high">High</option>
+                        <option value="medium">medium</option>
+                        <option value="low">low</option>
+                        </select>
+                    </div>
+                <div className="grid grid-cols-3 mt-5">
+                    <button type="button" className="btn btn-danger">cancel</button>
+                    <button type="submit" className="btn btn-primary">submit</button>
+                </div>
+                </form>
+                </Modal>
     );
 }
 
--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -48,13 +48,13 @@ export default function Modal({ isOpen, setIsOpen, title, children }) {
                   </div>
 
                   <div className="mt-4">
-                    <button
+                    {/* <button
                       type="button"
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
                       Got it, thanks!
-                    </button>
+                    </button> */}
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
