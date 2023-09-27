9efc6ec840edae72549e2ed0f4a348c167453cdd
tufaelkhan <tufaelkhan247@gmail.com>
2023-09-26 18:42:03 -0700

    add tasks

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,13 +2,15 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from '@heroicons/react/24/outline';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { userTasks } from '../../redux/features/tasks/tasksSlice';
+import { updateStatus, userTasks } from '../../redux/features/tasks/tasksSlice';
+import TaskDetailsModal from './TaskDetailsModal';
 
 const MyTasks = () => {
   const { tasks, userSpecificTasks } = useSelector((state) => state.tasksSlice)
   const { name: userName } = useSelector((state) => state.userSlice)
+  const [ isOpen, setIsOpen ] = useState(false)
   const dispatch = useDispatch()
 
   useEffect(()=>{
@@ -28,6 +30,7 @@ const MyTasks = () => {
 
   return (
     <div>
+      <TaskDetailsModal isOpen={isOpen} setIsOpen={setIsOpen}></TaskDetailsModal>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {
@@ -38,10 +41,12 @@ const MyTasks = () => {
         >
           <h1>{item.title}</h1>
           <div className="flex gap-3">
-            <button className="grid place-content-center" title="Details">
+            <button 
+            onClick={()=>setIsOpen(!isOpen)}
+            className="grid place-content-center" title="Details">
               <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
             </button>
-            <button className="grid place-content-center" title="Done">
+            <button onClick={()=>dispatch(updateStatus({id: item.id, status: 'done'}))} className="grid place-content-center" title="Done">
               <CheckIcon className="w-5 h-5 text-primary" />
             </button>
           </div>
---
+++ src/components/tasks/TaskDetailsModal.jsx
@@ -0,0 +1,13 @@
+import Modal from "../ui/Modal";
+
+function TaskDetailsModal({isOpen, setIsOpen}) {
+    return (
+        <div>
+           <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+            
+            </Modal> 
+        </div>
+    );
+}
+
+export default TaskDetailsModal;
